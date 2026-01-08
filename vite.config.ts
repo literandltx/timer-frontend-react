3f547a5f0a206b8d2fce85c896065c30203e2503
literandltx <romanskovronn@gmail.com>
2026-01-08 16:31:58 +0200

    in search of fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/timer/"
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        dedupe: ['react', 'react-dom', 'react-router-dom', 'react-router'],
+    },
+    base: "/timer/"
 })
