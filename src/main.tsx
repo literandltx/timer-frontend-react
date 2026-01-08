afddd6c9a79bb825ec7e6cdaf6c8aafed8373f0c
literandltx <romanskovronn@gmail.com>
2026-01-08 15:30:45 +0200

    add lock file, add 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home.tsx'
-import {BrowserRouter, Route, Routes} from "react-router";
 import History from "./pages/History.tsx";
 
 createRoot(document.getElementById('root')!).render(
@@ -9,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/history" element={<History/>}/>
+            <Route path="*" element={<div>404 Not Found</div>}/>
         </Routes>
-    </BrowserRouter>,
+    </BrowserRouter>
 )
