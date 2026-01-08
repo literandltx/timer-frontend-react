1dd2714b59cfacf62da70835714582e11b32360f
literandltx <romanskovronn@gmail.com>
2026-01-08 16:37:38 +0200

    final fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import History from "./pages/History.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <div>test</div>
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/history" element={<History/>}/>
