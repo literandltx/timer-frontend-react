6ca2af72d63ffb50d0f7d356faf896912faacc4e
literandltx <romanskovronn@gmail.com>
2026-01-23 13:58:21 +0200

    feat: delete redundant elements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,10 @@ import {NavLink} from "react-router";
 import Timer from "../components/home/Timer.tsx";
 import SettingModal from "../components/home/SettingModal.tsx";
 import LabelSelector from "../components/home/LabelSelector.tsx";
-import Counter from "../components/home/Counter.tsx";
 import {useLabels} from "../hooks/useLabels";
 import {useTimerSettings} from "../hooks/useTimerSettings";
 import {useTimerHistory} from "../hooks/useTimerHistory";
-import {useTabNotification} from "../hooks/useTabNotification"; // Import the new hook
+import {useTabNotification} from "../hooks/useTabNotification";
 import type {TimerData} from "../types/timer.ts";
 
 const SECONDS_PER_MINUTE = 60;
@@ -25,7 +24,7 @@ function Home() {
         addCustomOption,
         removeOption
     } = useTimerSettings();
-    const {history, addTimer} = useTimerHistory();
+    const {addTimer} = useTimerHistory();
 
     const timeAmount: number = selectedOption.value * SECONDS_PER_MINUTE;
     const {labels, activeLabel, handleLabelChange} = useLabels();
@@ -57,9 +56,9 @@ function Home() {
                     onChange={handleLabelChange}
                     options={labels}
                 />
-                <Counter count={history.length}/>
             </div>
             <div className="absolute bottom-[2%] left-[2%] flex items-center gap-2">
+                <NavLink to={"/history"}>History</NavLink>
                 <SettingModal
                     selectedOption={selectedOption}
                     availableOptions={availableOptions}
@@ -67,7 +66,6 @@ function Home() {
                     onAddCustom={addCustomOption}
                     onRemove={removeOption}
                 />
-                <NavLink to={"/history"}>History</NavLink>
             </div>
         </div>
     );
