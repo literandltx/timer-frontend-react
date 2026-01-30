7dd5878d0632ff1bd142e3465ff12f1210004292
literandltx <romanskovronn@gmail.com>
2026-01-30 22:07:30 +0200

    fix

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,7 +1,9 @@
+import type {Label} from "./labels.ts";
+
 export interface TimerOption {
     id: number;
     value: number;
-    label: string;
+    label: Label;
 }
 
 export interface UserSettings {
@@ -12,8 +14,36 @@ export interface UserSettings {
 }
 
 export const DEFAULT_OPTIONS: TimerOption[] = [
-    {id: 1, value: 20, label: '20 Min'},
-    {id: 2, value: 40, label: '40 Min'},
-    {id: 3, value: 60, label: '60 Min'},
-    {id: 4, value: 90, label: '90 Min'},
+    {
+        id: 1,
+        value: 20,
+        label: {
+            id: `default-20`,
+            name: '20 Min'
+        }
+    },
+    {
+        id: 2,
+        value: 40,
+        label: {
+            id: 'default-40',
+            name: '40 Min'
+        }
+    },
+    {
+        id: 3,
+        value: 60,
+        label: {
+            id: 'default-60',
+            name: '60 Min'
+        }
+    },
+    {
+        id: 4,
+        value: 90,
+        label: {
+            id: 'default-90',
+            name: '90 Min'
+        }
+    },
 ];
